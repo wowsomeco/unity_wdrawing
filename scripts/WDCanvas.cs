e9f3fb93c9e17028dceb0fe6c266cd66b0d8ed60
jim <irawan.jemmy@gmail.com>
2021-06-05 13:58:37 +0800

    init

---
+++ scripts/WDCanvas.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Wowsome.Chrono;
+using Wowsome.Tween;
+
+namespace Wowsome.Drawing {
+  [RequireComponent(typeof(Canvas))]
+  [RequireComponent(typeof(GraphicRaycaster))]
+  [RequireComponent(typeof(Image))]
+  [RequireComponent(typeof(Mask))]
+  public class WDCanvas : MonoBehaviour {
+    public delegate Color32 GetColor();
+
+    public RectTransform PointGroup;
+
+    List<RectTransform> _checkPoints = new List<RectTransform>();
+    RawImage _drawArea;
+    Texture2D _brush;
+    Texture2D _eraser;
+    Image _img;
+    RectTransform _rt;
+    Canvas _canvas;
+    Mask _mask;
+    PixelGrid _drawAreaPixels;
+    Color32[] _brushPixels;
+    Color32[] _eraserPixels;
+    bool _firstPaint = false;
+    bool _dragging = false;
+    Vector2 _lastDragPos = Vector2.zero;
+    float _drawLineThreshold = 8f;
+    // float _renderThreshold = 4f;
+    Checkpoint _checker;
+    Color32 _lastPaintColor;
+    Tweener _fader = null;
+    Timer _delayDone = null;
+    bool _disabled = false;
+
+    public Image Img { get { return _img; } }
+    public RectTransform Rt { get { return _rt; } }
+    public RawImage DrawArea { get { return _drawArea; } }
+    public GetColor OnPainting { get; set; }
+    public Action OnStartPainting { get; set; }
+    public Action OnEndedPainting { get; set; }
+    public Action ResetDelayDone { get; set; }
+    public Action OnDone { get; set; }
+
+    public bool Disabled {
+      get { return _disabled; }
+      set {
+        _disabled = value;
+        Img.raycastTarget = !_disabled;
+      }
+    }
+
+    public bool Active {
+      get { return gameObject.activeSelf; }
+      set {
+        _drawArea.enabled = value;
+        _img.enabled = value;
+        _canvas.enabled = value;
+        _mask.enabled = value;
+        gameObject.SetActive(value);
+
+        if (value) {
+          _img.SetAlpha(0f);
+          _fader = new Tweener(Tweener.FadeIn(_img.gameObject));
+          _fader.Play();
+        }
+      }
+    }
+
+    public List<Vector2> DotWorldPoints {
+      get { return _checkPoints.Map(rt => rt.transform.position.ToVector2()); }
+    }
+
+    public void Reactivate() {
+      _drawArea.enabled = true;
+      _img.enabled = true;
+      _canvas.enabled = true;
+      _mask.enabled = true;
+      Disabled = false;
+      _delayDone = null;
+    }
+
+    public bool OnTap(Vector2 screenPos) {
+      if (!Disabled && Paint(screenPos.ToLocalPos(_rt))) {
+        _delayDone = null;
+        Render();
+        return true;
+      }
+
+      return false;
+    }
+
+    public void OnStartSwipe(Vector2 screenPos) {
+      _delayDone = null;
+    }
+
+    public bool OnSwiping(Vector2 screenPos) {
+      if (Disabled) return false;
+      // dont render if outside the rect
+      bool isInsideRect = RectTransformUtility.RectangleContainsScreenPoint(_rt, screenPos, null);
+      if (!isInsideRect) return false;
+      bool painted = false;
+      // convert pos to anchored pos
+      Vector2 pos = screenPos.ToLocalPos(_rt);
+      float distance = Vector2.Distance(_lastDragPos, pos);
+      // optimization, dont render anything unless the distance to prev is not too close...
+      // comment it out for now
+      // if (distance < _renderThreshold) return;
+      // draw line logic here ... 
+      if (_firstPaint && _dragging) {
+        // draw line if distance is too far between delta                              
+        if (distance > _drawLineThreshold) {
+          float t = 0f;
+          while (t < 1f) {
+            t += 0.05f;
+            Paint(Vector2.Lerp(pos, _lastDragPos, t));
+          }
+        }
+      }
+      // last paint
+      painted = Paint(pos);
+      // cache to last drag
+      _lastDragPos = pos;
+      if (!_dragging) {
+        _dragging = true;
+        // broadcast ev start painting
+        if (null != OnStartPainting) OnStartPainting.Invoke();
+      }
+      // apply render only if painted
+      if (painted) {
+        Render();
+        if (!_firstPaint) _firstPaint = true;
+        // check if done
+        bool isEraser = _lastPaintColor.IsTransparent();
+        _checker.Check(pos, !isEraser);
+      }
+
+      return painted;
+    }
+
+    public void OnEndSwipe(Vector2 screenPos) {
+      if (Disabled) return;
+      _dragging = false;
+      // broadcast ended swipe
+      OnEndedPainting.Invoke();
+      // clean up on drag ends
+      Resources.UnloadUnusedAssets();
+      // broadcast done
+      if (_checker.Done) {
+        _delayDone = new Timer(2f);
+      }
+    }
+
+    public void InitCanvas(Texture2D textureBrush, Texture2D eraser) {
+      _img = GetComponent<Image>();
+      _rt = _img.rectTransform;
+      _canvas = GetComponent<Canvas>();
+      _mask = GetComponent<Mask>();
+      // convert size to int for better performance
+      VecInt size = new VecInt(_rt.Size() + new Vector2(10f, 10f));
+      // create draw area texture
+      _drawArea = ComponentExt.CreateComponent<RawImage>(_rt, "DrawArea");
+      _drawArea.rectTransform.Normalize().SetSize(size.ToVec2());
+      _drawArea.rectTransform.SetAsFirstSibling();
+      _drawArea.gameObject.SetActive(true);
+      // cache the brush pixel data
+      _brush = textureBrush;
+      _brushPixels = _brush.GetPixels32();
+      _eraser = eraser;
+      _eraserPixels = _eraser.GetPixels32();
+      // TODO: might need to tweak this as this is slow currently
+      _drawAreaPixels = new PixelGrid(size, new VecInt(Vector2.zero), Color.clear);
+      // fill with transparent color at the beginning      
+      Render();
+      // init check points
+      _checkPoints = PointGroup.GetComponentsWithoutSelf<RectTransform>(true);
+      _checker = new Checkpoint(_checkPoints);
+      // observe reset delay
+      ResetDelayDone += () => _delayDone = null;
+    }
+
+    public void UpdateCanvas(float dt) {
+      if (null != _fader) { _fader.Update(dt); }
+
+      if (null != _delayDone && !_delayDone.UpdateTimer(dt)) {
+        _delayDone = null;
+        SetDone();
+      }
+    }
+
+    public void SetDone() {
+      _delayDone = null;
+      Disabled = true;
+      OnDone.Invoke();
+    }
+
+    void Render() {
+      if (Disabled) return;
+      _drawArea.texture = _drawAreaPixels.GetTexture();
+    }
+
+    bool Paint(Vector2 draggingPos) {
+      if (Disabled) return false;
+
+      _lastPaintColor = OnPainting();
+      VecInt brushSize = GetBrushSize(_lastPaintColor);
+      VecInt pos = new VecInt(
+        new Vector2(
+          draggingPos.x - (brushSize.x / 2),
+          draggingPos.y - (brushSize.y / 2)
+        )
+      );
+
+      return _drawAreaPixels.Stamp(
+        brushSize,
+        pos,
+        _lastPaintColor.IsTransparent() ? _eraserPixels : _brushPixels,
+        _lastPaintColor
+      );
+    }
+
+    Vector2 BrushSize(bool isEraser) {
+      return new Vector2(isEraser ? _eraser.width : _brush.width, isEraser ? _eraser.height : _brush.height);
+    }
+
+    VecInt GetBrushSize(Color32 c) {
+      bool isEraser = c.IsTransparent();
+      Vector2 size = new Vector2(isEraser ? _eraser.width : _brush.width, isEraser ? _eraser.height : _brush.height);
+      return new VecInt(size);
+    }
+  }
+}
---
+++ scripts/WDPixelHelpers.cs
@@ -0,0 +1,318 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wowsome.Drawing {
+  public static class Ext {
+    public static Color32 TransparentWhite() {
+      return new Color32(255, 255, 255, 0);
+    }
+
+    public static Color32 White() {
+      return new Color32(255, 255, 255, 255);
+    }
+
+    public static Color32 Purple() {
+      // #a29bfe
+      return new Color32(162, 155, 254, 255);
+    }
+
+    public static Color32 Blue() {
+      // #74b9ff
+      return new Color32(116, 185, 255, 255);
+    }
+
+    public static Color32 Green() {
+      // #81ecec
+      return new Color32(129, 236, 236, 255);
+    }
+
+    public static Color32 Red() {
+      // #fab1a0
+      return new Color32(250, 177, 160, 255);
+    }
+
+    public static Color32 Yellow() {
+      // #ffeaa7
+      return new Color32(255, 234, 167, 255);
+    }
+
+    public static Color32 Rainbow(int idx) {
+      Color32[] seeds = new Color32[] { Blue(), Green(), Yellow() };
+      return seeds[idx];
+    }
+
+    public static Color32 WithAlpha(this Color32 color, byte a) {
+      return new Color32(color.r, color.g, color.b, a);
+    }
+
+    public static bool IsTransparent(this Color32 c) {
+      return c.a == 0;
+    }
+
+    public static bool LessThan(this Color32 c, byte number) {
+      return c.r < number && c.g < number && c.b < number;
+    }
+
+    public static byte Subtract(this byte b, int v) {
+      int n = b - v < 0 ? b + v : b - v;
+      return (byte)n;
+    }
+
+    public static Color32 Lighten(this Color32 color, int alpha) {
+      if (alpha > 250) return color;
+
+      float factor = (float)(alpha) / 255f;
+      factor += 0.6f;
+      factor = factor.Clamp(0.1f, 1f);
+
+      float r = (color.r / factor).Clamp(0f, 255f);
+      float g = (color.g / factor).Clamp(0f, 255f);
+      float b = (color.b / factor).Clamp(0f, 255f);
+
+      return new Color32((byte)r, (byte)g, (byte)b, color.a);
+    }
+
+    public static Color32 RandomizeAlpha(this Color32 c, int from = 0, int to = 255) {
+      return new Color32(
+        c.r,
+        c.g,
+        c.b,
+        (byte)Random.Range(from, to)
+      );
+    }
+
+    public static Vector2 ToLocalPos(this Vector2 screenPos, RectTransform parent, Camera camera = null) {
+      Vector2 pos;
+      RectTransformUtility.ScreenPointToLocalPointInRectangle(parent, screenPos, camera, out pos);
+      return pos;
+    }
+  }
+
+  public struct VecInt {
+    Vector2 _vector;
+
+    public int x { get { return Mathf.FloorToInt(_vector.x); } }
+    public int xHalf { get { return x / 2; } }
+    public int y { get { return Mathf.FloorToInt(_vector.y); } }
+    public int yHalf { get { return y / 2; } }
+    public int xy { get { return x * y; } }
+    public int Sum { get { return x + y; } }
+    public VecInt Half { get { return new VecInt(xHalf, yHalf); } }
+
+    public VecInt(Vector2 v) {
+      _vector = v;
+    }
+
+    public VecInt(int x, int y) {
+      _vector = new Vector2(x, y);
+    }
+
+    public override string ToString() {
+      return x + "," + y;
+    }
+
+    public override int GetHashCode() {
+      return (x << 16) | y;
+    }
+
+    public override bool Equals(object obj) {
+      if (!(obj is VecInt))
+        return false;
+
+      VecInt other = (VecInt)obj;
+      return x == other.x && y == other.y;
+    }
+
+    public Vector2 ToVec2() {
+      return new Vector2(x, y);
+    }
+  }
+
+  public class Pixel {
+    // public Color32 Color { get; set; }
+    public VecInt Pos { get; private set; }
+    public int Index { get; private set; }
+
+    public Pixel(/*Color32 color,*/ VecInt pos, int idx) {
+      // Color = color; 
+      Pos = pos;
+      Index = idx;
+    }
+  }
+
+  public class PixelGrid {
+    delegate Color32 GetColor(int idx);
+
+    public VecInt Pos { get; private set; }
+    public Dictionary<VecInt, Pixel> Pixels { get; private set; }
+    public VecInt Size { get; private set; }
+
+    Color32[] _colors = null;
+    Texture2D _texture = null;
+    // Rect _rect;
+
+    public PixelGrid(VecInt size, VecInt mid, Color32 color) {
+      // _rect = new Rect(mid.ToVec2(), size.ToVec2());
+      GeneratePixels(size, mid, idx => color);
+    }
+
+    public bool Stamp(VecInt size, VecInt pos, Color32[] pixels, Color32 color) {
+      bool stamped = false;
+
+      /*
+      // TODO: optimization
+      // 1. check if pos is within the rectangle of the texture
+      // 2. if yes and Pixels dist does not contain contain the pos key yet, then add to the dictionary.
+      // if (intersects) 
+      Rect r = new Rect(pos.ToVec2(), size.ToVec2());
+      bool intersects = _rect.Intersects(r);
+      if (intersects) {
+        Pixel pixel = null;
+        bool exists = Pixels.TryGetValue(pos, out pixel);
+        if (!exists) {
+          
+          Pixels[pos] = new Pixel(pos, idx);
+        }
+      }
+      */
+
+      Pixel pixel = null;
+      // get the collide pos first
+      // then iterate over the sides according to the stamp pixel size.      
+      if (Pixels.TryGetValue(pos, out pixel)) {
+        stamped = true;
+
+        int totalPixel = Pixels.Count;
+        int pixIdx = pixel.Index;
+        int i = 0;
+
+        for (int y = 0; y < size.y; ++y) {
+          bool isLeftEdge = false;
+          for (int x = 0; x < size.x; ++x) {
+            Color32 pixelColor = pixels[i];
+            // make sure the pixel idx isnt out of range
+            bool idxInRange = pixIdx < totalPixel;
+            if (!idxInRange) break;
+            // skip if left edge until the row (y) changes
+            // skip the transparent color, only stamp the black one            
+            if (!isLeftEdge && !pixelColor.IsTransparent()) {
+              if (!color.IsTransparent()) {
+                int alpha = pixelColor.a;
+                // if not transparent, add from the cur alpha
+                Color32 curColor = _colors[pixIdx];
+                if (!curColor.IsTransparent()) {
+                  alpha += (int)curColor.a;
+                  alpha = alpha.Clamp(0, 255);
+                }
+
+                _colors[pixIdx] = color.WithAlpha((byte)alpha);
+              } else {
+                // make smooth edge
+                int a = 255 - (int)pixelColor.a;
+                a = a.Clamp(0, 5);
+                Color32 curColor = _colors[pixIdx];
+                if (a > 0 && !curColor.IsTransparent()) {
+                  _colors[pixIdx] = curColor.WithAlpha((byte)a);
+                } else {
+                  _colors[pixIdx] = Ext.TransparentWhite();
+                }
+              }
+            }
+
+            ++pixIdx;
+            ++i;
+
+            if (!isLeftEdge) {
+              isLeftEdge = (pixIdx % Size.x == 0);
+              // revert prev
+              if (isLeftEdge) {
+                _colors[pixIdx - 1] = Ext.TransparentWhite();
+              }
+            }
+
+            bool last = (x == size.x - 1);
+            // shift up the index one row if last
+            if (last) {
+              pixIdx = pixIdx + Size.x - size.x;
+              isLeftEdge = false;
+            }
+          }
+        }
+      }
+
+      return stamped;
+    }
+
+    public Texture2D GetTexture() {
+      _texture.SetPixels32(_colors);
+      _texture.Apply();
+      return _texture;
+    }
+
+    void GeneratePixels(VecInt size, VecInt mid, GetColor getColor) {
+      Pos = mid;
+      Pixels = new Dictionary<VecInt, Pixel>();
+      Size = size;
+
+      VecInt startPos = new VecInt(mid.x - (size.x / 2), mid.y - (size.y / 2));
+
+      _texture = new Texture2D(Size.x, Size.y);
+      // this will generate transparent texture already.
+      // no need to iterate over at the bottom.
+      _colors = new Color32[Size.xy];
+
+      int i = 0;
+      // FIXME: this is terribly slow.
+      // need to find a better way to speed this up
+      for (int y = 0; y < size.y; ++y) {
+        for (int x = 0; x < size.x; ++x) {
+          VecInt pixelPos = new VecInt(startPos.x + x, startPos.y + y);
+          Pixels[pixelPos] = new Pixel(pixelPos, i);
+          ++i;
+        }
+      }
+    }
+  }
+
+  public class Checkpoint {
+    public class Point {
+      public bool Done { get; private set; }
+
+      Vector2 _pos;
+
+      public Point(Vector2 pos, RectTransform rt) {
+        _pos = pos;
+      }
+
+      public bool CheckDone(Vector2 dragPos, bool shouldDone) {
+        float dist = Vector2.Distance(dragPos, _pos);
+        // might want to make the leeway configurable via param later
+        bool isInDistance = dist < 40f;
+        if (isInDistance) {
+          if (shouldDone) {
+            if (!Done) Done = true;
+          } else {
+            Done = false;
+          }
+        }
+        return Done;
+      }
+    }
+
+    List<Point> _points = new List<Point>();
+
+    public bool Done { get; private set; }
+
+    public Checkpoint(List<RectTransform> points) {
+      points.ForEach(p => _points.Add(new Point(p.Pos(), p)));
+    }
+
+    public bool Check(Vector2 pos, bool shouldDone) {
+      Done = true;
+      foreach (Point p in _points) {
+        if (!p.CheckDone(pos, shouldDone)) Done = false;
+      }
+      return Done;
+    }
+  }
+}
